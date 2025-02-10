633b1d3ba288fdccd940839f11e7b969788a7764
shinnali <jungyouna12@naver.com>
2025-02-10 21:52:01 +0900

    [신나리] -추가

--- src/main/java/com/shinnal/spring/test/lifecycle/Movie.java
+++ src/main/java/com/shinnal/spring/test/lifecycle/Movie.java
@@ -1,5 +1,17 @@
 package com.shinnal.spring.test.lifecycle;
 
 public class Movie {
-
+	
+	private int rate;
+	private String director;
+	private int time;
+	private String title;
+	
+	public Movie(int rate, String director, int time, String title) {
+		this.rate = rate;
+		
+	}
+	
+	
+	
 }
